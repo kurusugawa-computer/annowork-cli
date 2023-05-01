c21aa16a0c9564623a47d527177eb824767c092e
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2023-05-01 14:09:30 +0900

    `annofab list_working_hours` : Annofab作業時間が0のときにエラーが発生する不具合を修正 (#133)
    * list_working_time: Annofab作業時間がzeroのときの不具合を修正
    
    * testu code酒精

--- tests/annofab/test_list_working_hours.py
+++ tests/annofab/test_list_working_hours.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import pandas
 
-from annoworkcli.annofab.list_working_hours import _get_get_df_working_hours_from_df
+from annoworkcli.annofab.list_working_hours import _get_df_working_hours_from_df
 
 # プロジェクトトップに移動する
 os.chdir(os.path.dirname(os.path.abspath(__file__)) + "/../../")
@@ -13,14 +13,14 @@ out_dir = Path("./tests/out/annofab/list_working_hours")
 out_dir.mkdir(exist_ok=True, parents=True)
 
 
-class Test__get_get_df_working_hours_from_df:
+class Test__get_df_working_hours_from_df:
     def test_normal(self):
         df_user_and_af_account = pandas.read_csv(str(data_dir / "user_and_af_account.csv"))
         df_job_and_af_project = pandas.read_csv(str(data_dir / "job_and_af_project.csv"))
         df_af_working_hours = pandas.read_csv(str(data_dir / "af_working_hours.csv"))
         df_actual_working_hours = pandas.read_csv(str(data_dir / "actual_working_hours.csv"))
 
-        df = _get_get_df_working_hours_from_df(
+        df = _get_df_working_hours_from_df(
             df_actual_working_hours=df_actual_working_hours,
             df_user_and_af_account=df_user_and_af_account,
             df_job_and_af_project=df_job_and_af_project,
