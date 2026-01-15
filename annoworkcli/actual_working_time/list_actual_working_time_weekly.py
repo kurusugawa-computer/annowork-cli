7af311a76dfc3973866baaf2e95f1db111917c13
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2026-01-15 10:44:47 +0900

    get_weekly_actual_working_hours_df関数のresampleメソッドの使用を修正し、DeprecationWarningを解消

--- annoworkcli/actual_working_time/list_actual_working_time_weekly.py
+++ annoworkcli/actual_working_time/list_actual_working_time_weekly.py
@@ -35,7 +35,9 @@ def get_weekly_actual_working_hours_df(actual_working_times: list[dict[str, Any]
         # DeprecationWarning: DataFrameGroupBy.resample operated on the grouping columns.
         # This behavior is deprecated, and in a future version of pandas the grouping columns will be excluded from the operation.
         # Either pass `include_groups=False` to exclude the groupings or explicitly select the grouping columns after groupby to silence this warning.  # noqa: E501
-        df.groupby("workspace_member_id").resample("W-SUN", on="dt_date", label="left", closed="left", include_groups=False).sum(numeric_only=True)
+        df.groupby("workspace_member_id")
+        .resample("W-SUN", on="dt_date", label="left", closed="left", include_groups=False)
+        .agg({"actual_working_hours": "sum"})
     )
     df_weekly.reset_index(inplace=True)
 
